884dc8a016c78b3d8aa44cb94cea8d2247a9f53f
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-11-29 19:30:54 +0200

    Finish position of Items in CalendarSection

--- src/components/CalendarSection/CalendarView/CalendarTable/index.js
+++ src/components/CalendarSection/CalendarView/CalendarTable/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Head from './Head'
 import Body from './Body';
 import styles from './CalendarTable.module.scss'
-import Head from './Head'
 
 export default function CalendarTable(props) {
   return (
-    <table className={styles.calendarTable}>
-      <Head />
-      <Body date={props.date} />
-    </table>
+    <div className={styles.tableContainer}>
+      <table className={styles.calendarTable}>
+        <Head />
+        <Body date={props.date} />
+      </table>
+    </div>
   )
 }
