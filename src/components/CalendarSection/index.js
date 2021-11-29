c7741ed1e76a5bc6280ed1434b003b9dabcb2a07
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-11-29 12:48:30 +0200

    Update style of Calendar

--- src/components/CalendarSection/index.js
+++ src/components/CalendarSection/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import CalendarView from './CalendarView'
 import CurrentDate from './CurrentDate'
-import styles from './CalendarView.module.scss'
+import styles from './CalendarSection.module.scss'
 
-export default function CalendaSection() {
+export default function CalendarSection() {
   return (
     <section className={styles.calendarSection}>
       <CurrentDate />
